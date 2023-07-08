9bf93e641f695dba2206a806c8811c4a10d643af
MrZ <87449476+mrz-2196f3@users.noreply.github.com>
2023-07-08 03:22:56 -0700

    Updated script.js -MRZ

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
-function heart(){
-    const heart = document.createElement("div");
-    heart.classList.add("heart");
-    heart.style.left=Math.random()*100+"vw";
-    heart.style.animationDuration=Math.random()*2+3;
-    heart.innerText="💖";
-    document.body.appendChild(heart);
+function snow(){
+    const snow = document.createElement("div");
+    snow.classList.add("snow");
+    snow.style.left=Math.random()*100+"vw";
+    snow.style.animationDuration=Math.random()*2+3;
+    snow.innerText="❄️";
+    document.body.appendChild(snow);
     setTimeout(() => {
-        heart.remove();
+        snow.remove();
     },3000)
 }
-function textHeart(){
-    const textHeart = document.createElement("div");
-    textHeart.classList.add("textHeart");
-    textHeart.style.left=Math.random()*105+"vw";
-    textHeart.style.animationDuration=Math.random()*2+3;
-    textHeart.innerText="mwa";
-    document.body.appendChild(textHeart);
+function snowMedium(){
+    const snowMedium = document.createElement("div");
+    snowMedium.classList.add("snowMedium");
+    snowMedium.style.left=Math.random()*105+"vw";
+    snowMedium.style.animationDuration=Math.random()*2+3;
+    snowMedium.innerText="❄️";
+    snowMedium.style.fontSize ="13px"
+    document.body.appendChild(snowMedium);
     setTimeout(() => {
-        textHeart.remove();
+        snowMedium.remove();
     },3000)
     
 }
-setInterval(textHeart, 200);
-
-setInterval(heart, 200);
+setInterval(snowMedium, 200);
+setInterval(snow, 200);
+//set background color
+document.body.style.background = "black";
